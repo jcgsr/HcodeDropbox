debe5cbb848c39726de92272882d8567c326526f
jcgsr <cabralguerra@gmail.com>
2020-10-09 06:01:23 -0300

    ajax

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -19,9 +19,47 @@ class DropBoxController {
 
 		this.inputFilesEl.addEventListener('change', event => {
 
-			console.log(event.target.files);
+			this.uploadTasks(event.target.files)
 
 			this.snackModalEl.style.display = 'block';
 		})
+	}
+
+	uploadTasks(files) {
+
+		let promises = [];
+
+		[...files].forEach(file => {
+
+			promises.push(new Promise((resolve, reject) => {
+
+				let ajax = new new XMLHttpRequest();
+
+				ajax.open('POST', '/upload');
+
+				ajax.onload = event => {
+
+					try {
+						resolve(JSON.parse(ajax.responseText));
+					} catch(e) {
+						reject(e);
+					}
+				};
+
+				ajax.onerror = event => {
+
+					reject(event);
+
+				};
+
+				let formData = new FormData();
+
+				formData.append('input-file', file)
+
+				ajax.send(formData)
+			}));
+		})
+
+		return Promise.all(promises);
 	}
 }
